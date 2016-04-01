856128ec516f996ca941036c514532f6748cada2
sniffy1988 <sniffy1988@gmail.com>
2016-04-01 19:44:29 +0300

    forgot (((

--- src/app/components/Property.component.ts
+++ src/app/components/Property.component.ts
@@ -4,52 +4,33 @@ import {NestoriaAPI} from '../services/nestoriaAPI.service';
 import {ItemsComponent} from './items.component';
 
 @Component({
-    selector: 'property',
-    providers: [JSONP_PROVIDERS, NestoriaAPI],
-    directives: [ItemsComponent],
-    template: `<input type="text" [(ngModel)]="searchQuery">
+  selector: 'property',
+  providers: [JSONP_PROVIDERS, NestoriaAPI],
+  directives: [ItemsComponent],
+  template: `<input type="text" [(ngModel)]="searchQuery">
                 <button (click)="search(searchQuery)">Go</button>
                 <items-list [itemData]="items.data" *ngIf="items" [itemType]="items.type"></items-list>
                 `
 })
 
 export class PropertyComponent {
-    searchQuery:string;
-    items;
+  searchQuery:string;
+  items;
 
-    constructor(private _api:NestoriaAPI) {
-        this.searchQuery = '';
-    }
+  constructor(private _api:NestoriaAPI) {
+    this.searchQuery = '';
+  }
 
-    search(query:string) {
+  search(query:string) {
+    //noinspection TypeScriptUnresolvedVariable
+    console.log(query);
+    let response = this._api.fetch(query);
+    response.then(data => {
+        this.items = data;
+      })
+      .catch(err => {
         //noinspection TypeScriptUnresolvedVariable
-        console.log(query);
-        const validCodes = ['100', '101', '110'],
-            ambiguousCodes = ['200', '202'];
-        let response = this._api.fetch(query);
-        response
-            .subscribe(
-                data => {
-                    //noinspection TypeScriptUnresolvedVariable
-                    let response = data['_body'].response;
-                    let responseCode = response.application_response_code;
-                    //noinspection TypeScriptUnresolvedFunction
-                    if (validCodes.indexOf(responseCode) > -1) {
-                        this.items = {
-                            type: 'listings',
-                           data:  response.listings
-                        };
-                    } else { //noinspection TypeScriptUnresolvedFunction
-                        if (ambiguousCodes.indexOf(responseCode) > -1) {
-                            this.items = {
-                                type:'locations',
-                                data: response.locations
-                            };
-                        }
-                    }
-                    //noinspection TypeScriptUnresolvedVariable
-                    console.log(this.items);
-                }
-            );
-    }
+        console.log(err);
+      })
+  }
 }
